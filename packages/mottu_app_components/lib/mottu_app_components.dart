955e1e17ad111ee3254e17b057b1b07f27d0521f
Ariadni Chioquetta <ariadni.chioquetta@colab.re>
2022-06-17 23:51:38 -0300

    add export module

---
+++ packages/mottu_app_components/lib/mottu_app_components.dart
@@ -0,0 +1,18 @@
+library mottu_app_components;
+
+export 'src/deliveries/deliveries_factory.dart';
+export 'src/deliveries/domain/deliveries_repository.dart';
+export 'src/deliveries/presentation/deliveries_localization.dart';
+
+export 'src/earnings/earnings_factory.dart';
+export 'src/earnings/domain/earnings_repository.dart';
+export 'src/earnings/presentation/earnings_localization.dart';
+
+export 'src/next_transfer/next_transfer_factory.dart';
+export 'src/next_transfer/presentation/next_transfer_localization.dart';
+
+export 'src/search_drivers/search_drivers_factory.dart';
+export 'src/search_drivers/presentation/search_drivers_localization.dart';
+
+export 'src/status/status_factory.dart';
+export 'src/status/presentation/status_localization.dart';
